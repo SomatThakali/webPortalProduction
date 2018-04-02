aadcd8c800bee2b0837987a7fde484980cf233d4
kevincall <kevin.call@outlook.com>
2018-04-02 01:04:19 -0400

    encapuslated view calls for the portal app. removed from main page.

---
+++ patientPortal/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+
+from . import views
+
+urlpatterns = [
+    path('',include('django.contrib.auth.urls')),
+    path('patient/information',views.information),
+    path('patient/myprogress',views.myprogress),
+]
--- website/urls.py
+++ website/urls.py
@@ -1,15 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from patientPortal.views import Signup
-from patientPortal.views import information
-from patientPortal.views import myprogress
+#from patientPortal.views import Signup
+#from patientPortal.views import information
+#from patientPortal.views import myprogress
 
 urlpatterns = [
-    path('Signup/', Signup),
-    path('myprogress/', myprogress),
-    path('information/', information),
+    path('portal/', include('patientPortal.urls')),
     path('admin/', admin.site.urls),
 ]
 
