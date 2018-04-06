d9dd16e2b897eba4f57977213561c7fb88a9bd66
kevincall <kevin.call@outlook.com>
2018-04-06 14:01:47 -0400

    Temporary solution to prevent users from accessing pages unless loggedin

--- patientPortal/views.py
+++ patientPortal/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from. forms import MyPersonalInformationForm
+from django.conf import settings
+from django.shortcuts import redirect
+
+
 # Create your views here.
 
 
@@ -11,16 +15,27 @@ def MyPersonalInformation(request):
         save_it.save()
     return render(request, 'patientPortal/information.html')
 
-
 def myprogress(request):
+    if not request.user.is_authenticated:
+        return redirect('/portal/login')
     return render_to_response('patientPortal/myprogress.html')
 
-
 def patientDashboard(request):
+    if not request.user.is_authenticated:
+        return redirect('/portal/login')
     return render_to_response('patientPortal/patientDashboard.html')
 
 def exercise(request):
+    if not request.user.is_authenticated:
+        return redirect('/portal/login')
     return render_to_response('patientPortal/exercise.html')
 
 def patientCalendar(request):
+    if not request.user.is_authenticated:
+        return redirect('/portal/login')
     return render_to_response('patientPortal/patientCalendar.html')
+
+def dispatch(request):
+    if not request.user.is_authenticated:
+        return redirect('/portal/login')
+    return render_to_response('patientPortal/patientDashboard.html')
