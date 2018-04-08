17a25282cb8dcce1cb2137d55ee619ceb019292e
kevincall <kevin.call@outlook.com>
2018-04-08 01:51:03 -0400

    Method to prevent patients from therapist pages, and vice versa

--- patientPortal/views.py
+++ patientPortal/views.py
@@ -3,73 +3,114 @@ from django.shortcuts import render_to_response
 from. forms import MyPersonalInformationForm
 from django.conf import settings
 from django.shortcuts import redirect
+from .accessoryScripts.checkGroup import is_patient, is_therapist
 
 
 # PATIENT VIEWS #
 def patientDashboard(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/patientDashboard.html')
+    if is_patient(request.user):
+        user = request.user
+        first_name = user.first_name
+        last_name = user.last_name
+        return render(
+            request,'patientPortal/patientDashboard.html',context={'first_name': first_name, 'last_name': last_name},
+            )
+    else:
+        return redirect('/portal/therapist')
 
 def MyPersonalInformation(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    form = MyPersonalInformationForm(request.POST or None)
-    if form. is_valid():
-        save_it = form.save(commit=False)
-        save_it.save()
-    return render(request, 'patientPortal/information.html')
+    if is_patient(request.user):
+        form = MyPersonalInformationForm(request.POST or None)
+        if form. is_valid():
+            save_it = form.save(commit=False)
+            save_it.save()
+        return render(request, 'patientPortal/information.html')
+    else:
+        return redirect('/portal/therapist')
 
 def myprogress(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/myprogress.html')
+    if is_patient(request.user):
+        return render_to_response('patientPortal/myprogress.html')
+    else:
+        return redirect('/portal/therapist')
 
 def exercise(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/exercise.html')
+    if is_patient(request.user):
+        return render_to_response('patientPortal/exercise.html')
+    else:
+        return redirect('portal/therapist')
 
 def patientCalendar(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/patientCalendar.html')
+    if is_patient(request.user):
+        return render_to_response('patientPortal/patientCalendar.html')
+    else:
+        return redirect('portal/therapist')
 
 # THERAPIST VIEWS #
 def therapistDashboard(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/therapistDashboard.html')
+    if is_therapist(request.user):
+        return render_to_response('patientPortal/therapistDashboard.html')
+    else:
+        return redirect('portal/patient')
 
 def therapistCalendar(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/therapistCalendar.html')
+    if is_therapist(request.user):
+        return render_to_response('patientPortal/therapistCalendar.html')
+    else:
+        return redirect('portal/patient')
 
 def database(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/database.html')
+    if is_therapist(request.user):
+        return render_to_response('patientPortal/database.html')
+    else:
+        return redirect('portal/patient')
 
 def recruitment(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/recruitment.html')
+    if is_therapist(request.user):
+        return render_to_response('patientPortal/recruitment.html')
+    else:
+        return redirect('portal/patient')
 
 def forms(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/forms.html')
+    if is_therapist(request.user):
+        return render_to_response('patientPortal/forms.html')
+    else:
+        return redirect('portal/patient')
 
 def settings(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/settings.html')
-
-
+    if is_therapist(request.user):
+        return render_to_response('patientPortal/settings.html')
+    else:
+        return redirect('portal/patient')
 
 # FIXME must redirect to the appropriate dashboard
 def dispatch(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
-    return render_to_response('patientPortal/patientDashboard.html')
+    if is_patient(request.user):
+        return redirect('/portal/patient')
+    if is_therapist(request.user):
+        return redirect('/portal/therapist')
+    return redirect('/admin')
