e619b8f563cf842fcd39d45857f3323fe45888bd
somat thakali <somatthakali@somats-MBP.nyc.rr.com>
2018-04-14 16:08:57 -0400

    modified modification model and added in views.py

--- patientPortal/views.py
+++ patientPortal/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from. forms import MyPersonalInformationForm
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.conf import settings
 from django.shortcuts import redirect
 from .accessoryScripts.checkGroup import is_patient, is_therapist
-from django.http import HttpResponse
+from. forms import MyPersonalInformationForm
 import json
+from .models import notification
 # PATIENT VIEWS #
+
+
 def patientDashboard(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
@@ -67,6 +71,17 @@ def therapistDashboard(request):
     if not request.user.is_authenticated:
         return redirect('/portal/login')
     if is_therapist(request.user):
+        def show_notifications(request, notifications_id):
+            n = notification.objects.get(id=notifications_id)
+            return render_to_response('patientPortal/therapistDashboard.html', {'notifications': n})
+
+        def delete_notifications(request, notifications_id):
+            n = notification.objects.get(id=notifications_id)
+            n.viewed = True
+            n.save()
+            n1 = notification.objects.filter(user=request.user, viewed=False)
+            return HttpResponseRedirect('/portal/patient', {'notifications': n1})
+
         return render_to_response('patientPortal/therapistDashboard.html')
     else:
         return redirect('/portal/patient')
@@ -104,32 +119,31 @@ def forms(request):
         return redirect('/portal/login')
     if is_therapist(request.user):
         from patientPortal.apiScripts.exports import get_available_forms
-        method = request.method;
+        method = request.method
         if (method == "GET"):
-            request_query_dict = request.GET;
-            request_dict = dict(request_query_dict);
-            if not bool(request_dict): # The dict is empty means request has been made to load page
-                forms = get_available_forms();
-                return render(request, 'patientPortal/forms.html',  context = {'forms' : forms})
+            request_query_dict = request.GET
+            request_dict = dict(request_query_dict)
+            if not bool(request_dict):  # The dict is empty means request has been made to load page
+                forms = get_available_forms()
+                return render(request, 'patientPortal/forms.html',  context={'forms': forms})
 
             # This call only needs to be done here, keeping it local
             from patientPortal.apiScripts.exports import get_form_groups
 
             # on JSON creation on front end, turns these values for the keys into lists
             # taking first element of the list fixes this issue
-            form = request_dict['form'][0];
-            event = request_dict['event'][0];
-            question_groups = get_form_groups(form,event);
+            form = request_dict['form'][0]
+            event = request_dict['event'][0]
+            question_groups = get_form_groups(form, event)
             print(len(question_groups))
-            response_body = {'guestion_groups': question_groups, 'event': event};
-            return HttpResponse(json.dumps(response_body));
+            response_body = {'guestion_groups': question_groups, 'event': event}
+            return HttpResponse(json.dumps(response_body))
     else:
         return redirect('/portal/patient')
 
-
-    #print(bool(request_dict))
-    #forms = get_available_forms();
-    #return render(request, 'patientPortal/forms.html',  context = {'forms' : forms})
+    # print(bool(request_dict))
+    # forms = get_available_forms();
+    # return render(request, 'patientPortal/forms.html',  context = {'forms' : forms})
 
 
 def settings(request):
