eb9346e803ad71af572158b78b98a3526a178803
kevincall <kevin.call@outlook.com>
2018-04-16 19:28:23 -0400

    Fixing some bugs

--- patientPortal/apiScripts/helper.py
+++ patientPortal/apiScripts/helper.py
@@ -1,4 +1,4 @@
 def create_redcap_event_name(event_prefix, cohort_num):
     arr = [event_prefix, 'arm',str(cohort_num)]
-    redcap_event_name = ".".join(arr)
+    redcap_event_name = "_".join(arr)
     return redcap_event_name
