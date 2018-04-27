ddfb6249b755067f232b813eae7f021e9abf5877
kevincall <kevin.call@outlook.com>
2018-04-27 03:45:12 -0400

    added fixDict2 to prevent unnecessary API call to redcap

--- patientPortal/accessoryScripts/resourceManager.py
+++ patientPortal/accessoryScripts/resourceManager.py
@@ -3,12 +3,15 @@ def removeDictKey(dict, key):
     newDict = copy.deepcopy(dict)
     del newDict[key]
     return newDict;
-
+def fixDict2(dict):
+    for key in dict:
+        if len(dict[key]) == 1:
+            dict[key] = dict[key][0]
+    return dict 
 def fixDict(dict):
     from patientPortal.apiScripts.exports import get_all_questions
     questions = get_all_questions();
     checkbox_fields = [question['field_name'] for question in questions if question['field_type'] == 'checkbox']
-
     removed_checkboxes = [];
     added_keys = [];
     for key in dict:
